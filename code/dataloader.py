07c15692bf76f96afba62d1afaeaf9e18a874596
Tom Lotze <tom.lotze@student.auc.nl>
2020-09-18 11:50:01 +0200

    working version, using only aggregated query and question as input

--- code/dataloader.py
+++ code/dataloader.py
@@ -2,7 +2,7 @@
 # @Author: TomLotze
 # @Date:   2020-09-15 01:35
 # @Last Modified by:   TomLotze
-# @Last Modified time: 2020-09-17 13:46
+# @Last Modified time: 2020-09-18 11:47
 
 import csv
 import torch
@@ -17,15 +17,8 @@ import pickle
 
 
 
-def get_embedding(inp, model="Albert"):
+def get_embedding(inp, tokenizer, model):
     """Find the embeddings for a string input, using a specified model"""
-    # decide which language model to use
-    if model == "Albert":
-        tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
-        model = AlbertModel.from_pretrained('albert-base-v2', return_dict=True)
-    else:
-        raise Exception(f"This model type ({model}) is not known")
-
     # tokenize the raw input
     tokenized_input = tokenizer(inp, return_tensors="pt")
 
@@ -33,7 +26,8 @@ def get_embedding(inp, model="Albert"):
     output = model(**tokenized_input)
 
     # retrieve the embedding from the model
-    embedding = output.last_hidden_state
+    embedding = output.last_hidden_state.detach()
+
 
     return embedding
 
@@ -49,33 +43,41 @@ with open("Data/MIMICS-Click.tsv") as tsvfile:
 headers = data[0]
 data = data[1:]
 
+# set language model
+model = "Albert"
+if model == "Albert":
+        tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+        model = AlbertModel.from_pretrained('albert-base-v2', return_dict=True)
+else:
+    raise Exception(f"This model type ({model}) is not known")
 
-dataloader = []
+
+dataset = []
 
 for i, (query, question, op1, op2, op3, op4, op5,
         impression_lvl, engagement_lvl, ccp1, ccp2,
         ccp3, cpp4, ccp5) in enumerate(data):
-
+    if i % 500 == 0:
+        print(i)
     # convert to right types
     impression_lvl = {"low": 1, "medium": 2, "high": 3}[impression_lvl]
-    engagement_lvl = torch.Tensor([int(engagement_lvl)]).int()
+    engagement_lvl = torch.Tensor([int(engagement_lvl)]).float()
     ccp1, ccp2, ccp3, cpp4, ccp5 = float(ccp1), float(ccp2), float(ccp3), float(cpp4), float(ccp5)
 
-    query = get_embedding(query)
-    question = get_embedding(question)
+    query = torch.mean(get_embedding(query, tokenizer, model), 1)
+    question = torch.mean(get_embedding(question, tokenizer, model), 1)
 
-    breakpoint()
+    q = torch.cat((query, question), 1)
 
-    nr_options = len([op for op in [op1, op2, op3, op4, op5] if op])
+    # nr_options = len([op for op in [op1, op2, op3, op4, op5] if op])
+    # batch_tensor = (query, question, nr_options, impression_lvl)
 
-    batch_tensor = (query, question, nr_options, impression_lvl)
-
-    # all datatypes must be converted to torch tensors,
-    dataloader.append((batch_tensor, engagement_lvl))
-
-    breakpoint()
+    # Add the datapoint to the dataset
+    dataset.append((q, engagement_lvl))
 
 
+# convert to pytorch dataloader
+dataloader = DataLoader(dataset, batch_size=56, shuffle=True)
 
 
 # save the dataloader
