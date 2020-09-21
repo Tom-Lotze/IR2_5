977990d39a56061ac5c680eec7216484766d4331
Tom Lotze <lgpu0009@test3.irc.sara.nl.irc.sara.nl>
2020-09-21 14:03:01 +0200

    running on scratch

--- scripts/dataloader_script.sh
+++ scripts/dataloader_script.sh
@@ -5,8 +5,16 @@
 
 source activate ir2
 
+
+cp -r $HOME/IR2_5/ $TMPDIR/
+
+cd $TMPDIR/IR2_5
+
 echo "Dataloader started running" | mail $USER
 python code/dataloader_sentence.py
+
+cp -r  $TMPDIR/IR2_5/Data/ $HOME/IR2_5/dataloader/
+
 
 echo "dataloader finished" | mail $USER
 
