0e739e45ffc23bc6a729680ec61df02263262f71
Tom Lotze <tom.lotze@student.auc.nl>
2020-09-22 10:40:23 +0200

    added validation and test set, script is saving a loss plot

--- code/train_regression.py
+++ code/train_regression.py
@@ -2,7 +2,7 @@
 # @Author: TomLotze
 # @Date:   2020-09-18 11:21
 # @Last Modified by:   TomLotze
-# @Last Modified time: 2020-09-21 17:10
+# @Last Modified time: 2020-09-22 10:38
 
 
 import argparse
@@ -10,7 +10,7 @@ import numpy as np
 import os
 from regression import Regression
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 import matplotlib.pyplot as plt
 import pickle as pkl
 
@@ -48,9 +48,21 @@ def train():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Device :", device)
 
-     # extract training data
+     # extract all data and divide into train, valid and split dataloaders
     with open(os.path.join(FLAGS.data_dir, "dataset.p"), "rb") as f:
-        dataloader = DataLoader(pkl.load(f), batch_size=64, shuffle=True)
+        dataset = pkl.load(f)
+
+    len_all = len(dataset)
+
+    train_len, valid_len = int(0.8 * len_all), int(0.05 * len_all)
+    test_len = len_all - train_len - valid_len
+    splits = [train_len, valid_len, test_len]
+    train_data, valid_data, test_data = random_split(dataset, splits)
+
+    train_dl = DataLoader(train_data, batch_size=64, shuffle=True)
+    valid_dl = DataLoader(valid_data, batch_size=64, shuffle=True, drop_last=True)
+    test_dl = DataLoader(test_data, batch_size=64, shuffle=True, drop_last=True)
+
 
      # initialize MLP and loss function
     nn = Regression(5376, dnn_hidden_units, 1, FLAGS.neg_slope).to(device)
@@ -74,16 +86,17 @@ def train():
 
     # initialization for plotting and metrics
     training_losses = []
-    test_losses = []
+    valid_losses = []
 
     # training loop
     for epoch in range(FLAGS.nr_epochs):
 
         print(f"\n\nEpoch: {epoch}")
+        batch_losses = []
 
         for batch, (x, y) in enumerate(dataloader):
 
-            batch_loss = []
+
 
             # squeeze the input, and put on device
             x = x.reshape(x.shape[0], -1).to(device)
@@ -103,84 +116,64 @@ def train():
             # update the weights
             optimizer.step()
 
-
-            # # evaluation on test set
-            # if step % FLAGS.eval_freq == 0:
-            #     test_accuracies, test_losses = eval_on_test(nn, loss_function, x_test, y_test, test_accuracies, test_losses)
-
-            # save training loss and print
-            training_losses.append(loss.item())
-            batch_loss.append(loss.item())
-            # if batch % 10 == 0:
-                # print(f"Training loss batch {batch}: {loss.item()}")
-
-        print(f"Average batch loss: {np.mean(batch_loss)}")
+            # save training loss
+            batch_losses.append(loss.item())
 
 
-        torch.save(nn.state_dict(), f"Models/Regression_{epoch}.pt")
+        avg_epoch_loss = np.mean(batch_losses)
+        training_losses.append(avg_epoch_loss)
+        print(f"Average batch loss (epoch {epoch}: {avg_epoch_loss} ({len(batch_losses)} batches).")
+
+        # get loss on validation set and evaluate
+        valid_losses.append(eval_on_test(nn, loss_function, valid_dl))
+        torch.save(nn.state_dict(), f"Models/Regression.pt")
 
 
-    # # compute loss and accuracy on the test set a final time
-    # test_accuracies, test_losses = eval_on_test(nn, loss_function, x_test, y_test, test_accuracies, test_losses)
-    # print("Maximum accuracy :", max(test_accuracies), "after %d steps\n"%(np.argmax(test_accuracies) * FLAGS.eval_freq))
+    # compute loss and accuracy on the test set
+    test_loss = eval_on_test(nn, loss_function, test_dl)
+    print(f"Loss on test set: {test_loss}")
 
-    # plotting(training_losses, test_losses, training_accuracies, test_accuracies, dnn_hidden_units, max(test_accuracies))
+    plotting(train_losses, valid_losses, test_loss)
 
-def eval_on_test(nn, loss_function, x_test, y_test, test_accuracies, test_losses):
+
+
+def eval_on_test(nn, loss_function, dl):
     """
     Find the accuracy and loss on the test set, given the current weights
     """
-    test_pred = nn(x_test)
-    true_labels = torch.max(y_test, 1)[1]
-    test_acc = accuracy(test_pred, y_test)
-    test_loss = loss_function(test_pred, true_labels)
-    print("Test accuracy is:", test_acc, "\n")
-    test_accuracies.append(test_acc)
-    test_losses.append(test_loss)
+    nn.eval()
+    with torch.no_grad():
+        losses = []
+        for (x, y) in dl:
+            test_pred = nn(x_test)
+            loss = loss_function(test_pred, y)
+            losses.append(loss.item())
 
-    return test_accuracies, test_losses
+    return np.mean(losses)
 
 
-def plotting(train_losses, test_losses, train_accuracies, test_accuracies, dnn_hidden_units, max_test):
+def plotting(train_losses, valid_losses, test_loss):
 
     os.makedirs("Images", exist_ok=True)
 
     plt.figure(figsize=(15, 12))
-    steps_all = np.arange(1, FLAGS.max_steps+1)
-    steps_test =  np.arange(0, FLAGS.max_steps+1, FLAGS.eval_freq)
-
-    # plot the accuracies
-    plt.subplot(2, 1, 1)
-    plt.plot(steps_all, train_accuracies, '-', lw=2, label="Train accuracy")
-    plt.plot(steps_test, test_accuracies, '-', lw=2, label="Test accuracy")
-
-    plt.xlabel('Step')
-    plt.ylabel('Accuracy')
-    plt.title('Accuracy for training and test data')
-    plt.grid(True)
-    plt.legend()
+    steps_all = np.arange(1, len(train_losses))
 
     # plot the losses
-    plt.subplot(2, 1, 2)
     plt.plot(steps_all, train_losses, '-', lw=2, label="Training loss")
-    plt.plot(steps_test, test_losses, '-', lw=2, label="Test loss")
-    plt.title('Loss for training and test data')
+    plt.plot(steps_all, valid_losses, '-', lw=2, label="Validation loss")
+    plt.hline(test_loss, 0, max(steps_all), label="Test loss")
+    plt.title('Losses over training, including final test loss')
 
-    plt.xlabel('Step')
-    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('MSE Loss')
     plt.grid(True)
     plt.legend()
 
     plt.tight_layout()
 
-    dnn_string = "_".join([str(i) for i in dnn_hidden_units])
-
-    fig_name = "max_accuracy="+str(max_test)+"_MLP_Torch_lr="+str(FLAGS.learning_rate)+ \
-    "_neg_sl="+str(FLAGS.neg_slope)+ "_optimizer="+ FLAGS.optimizer+ \
-    "_amsgrad="+str(int(FLAGS.amsgrad))+"_weightdecay=" + \
-    str(FLAGS.weightdecay)+ "_momentum="+str(FLAGS.momentum)+"_dnn="+dnn_string+".png"
-
-    plt.savefig("./images/"+"best"+fig_name)
+    fig_name = "loss_plot_regression.png"
+    plt.savefig(f"Images/{fig_name}")
 
 def print_flags():
       """
