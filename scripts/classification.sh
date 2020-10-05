15904ca1c314cab40ad6bae14ee6a602d8eeabf6
Tom Lotze <lgpu0009@test3.irc.sara.nl.irc.sara.nl>
2020-10-05 10:59:39 +0200

    removing one dimension from targets

--- scripts/classification.sh
+++ scripts/classification.sh
@@ -16,7 +16,7 @@ mkdir -p Images
 
 echo "Classification started running" | mail $USER
 
-python code/train_classifier.py --nr_epochs 15 --weightdecay 0.0001 --optimizer Adam --amsgrad 1
+python code/train_classifier.py --nr_epochs 150 --weightdecay 0.0001 --optimizer Adam --amsgrad 1
 
 cp -r Models/* $HOME/IR2_5/Models/
 cp -r Images/* $HOME/IR2_5/Images/
