e034d765e42ffda76e016680ee1aef0a9b9a336e
Tom Lotze <tom.lotze@student.auc.nl>
2020-10-12 19:05:35 +0200

    changed some parameters

--- scripts/classification.sh
+++ scripts/classification.sh
@@ -16,8 +16,7 @@ mkdir -p Images
 
 echo "Classification started running" | mail $USER
 
-python code/train_classifier.py --nr_epochs 100 --weightdecay 0.0001 --optimizer Adam --amsgrad 1 \
-	 --batchnorm 1 --dropout_probs "0.3, 0.05"
+python code/train_classifier.py --nr_epochs 100 --weightdecay 0.0001 --optimizer Adam --batchnorm 1 --dropout_probs "0.3, 0.05" --embedder "Bert"
 
 cp -r Models/* $HOME/IR2_5/Models/
 cp -r Images/* $HOME/IR2_5/Images/
