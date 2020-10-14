67d87e35ce73acd452066c55c627faca5f21d737
Tom Lotze <tom.lotze@student.auc.nl>
2020-10-14 14:53:31 +0200

    elaborate tuning script

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -23,7 +23,7 @@ echo "Results file" > results_tuning.txt
 echo "starting hyperparam tuning" | mail $USER
 
 declare -a dnn=("2000, 100, 16" "300, 32" "248")
-declare -a dropouts=("0.3, 0.2, 0.1" "0.3, 0.1" "0.1")
+declare -a dropouts=("0.3, 0.2, 0.1" "0.3, 0.1" "0.1" "0")
 declare -a lrs=("0.001" "0.0001")
 declare -a weightdecays=("0" "0.00001")
 declare -a embedders=("Bert" "TFIDF")
@@ -34,17 +34,22 @@ for optimizer in 'Adam' 'SGD'
 do
   for emb in "${embedders[@]}"
   do
+
     for learning_rate in "${lrs[@]}"
     do
       for i in $(seq 0 2)
       do
         for wd in "${weightdecays[@]}"
         do
-          python code/train_regression.py --nr_epochs 40 \
-          --optimizer "$optimizer" --weightdecay "$wd"\
-            --learning_rate "$learning_rate" --verbose 0 --embedder "$emb"\
-            --dnn_hidden_units "${dnn[$i]}" --dropout_probs \
-            "${dropouts[$i]}"
+          for ams in "0" "1"
+          do
+            for mom in "0" "0.9"
+            python code/train_regression.py --nr_epochs 35 \
+              --optimizer "$optimizer" --weightdecay "$wd" --amsgrad "$ams"\
+              --learning_rate "$learning_rate" --verbose 0 --embedder "$emb"\
+              --dnn_hidden_units "${dnn[$i]}" --dropout_probs \
+              "${dropouts[$i]}" --momentum "$mom"
+            done
           done
        done
      done
