2f018df7426c231ec196d9a41610609f53aa7600
Tom Lotze <lgpu0009@test3.irc.sara.nl.irc.sara.nl>
2020-10-14 15:07:41 +0200

    BERT to bert

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -18,7 +18,7 @@ echo "Regression started running" | mail $USER
 python code/test_cuda_print.py
 python code/train_regression.py --nr_epochs 40 --weightdecay 0.0001 \
     --optimizer SGD --momentum 0.9  --batchnorm 1 \
-    --dropout_probs "0.3, 0.05" --embedder 'BERT'
+    --dropout_probs "0.3, 0.05" --embedder 'Bert'
 
 cp -r Models/* $HOME/IR2_5/Models/
 cp -r Images/* $HOME/IR2_5/Images/
