63c39b7a69eaeead04682d91500683ff97e0311d
Tom Lotze <tom.lotze@student.auc.nl>
2020-10-21 10:14:05 +0200

    compare regression and classification

--- code/significance_testing.py
+++ code/significance_testing.py
@@ -2,7 +2,7 @@
 # @Author: TomLotze
 # @Date:   2020-10-15 15:09
 # @Last Modified by:   TomLotze
-# @Last Modified time: 2020-10-16 13:28
+# @Last Modified time: 2020-10-16 14:01
 
 from scipy.stats import ttest_rel
 import numpy as np
@@ -11,10 +11,14 @@ import pickle as pkl
 
 # difference between classification and regression on test set
 
+np.random.seed(42)
+
+
+
 for reduced_classes in [True, False]:
     for impression in [True, False]:
         for embedder in ["Bert", "TFIDF"]:
-            print(f"Embedder: {embedder}\nreduced_class: {reduced_classes}\nimpression:{impression}")
+            print(f"\n\nEmbedder: {embedder}\nreduced_class: {reduced_classes}\nimpression:{impression}")
 
             with open(f"Predictions/regression_test_preds{embedder}_{reduced_classes}_{impression}.pt", "rb") as f:
                 test_pred_regression = pkl.load(f)
@@ -26,8 +30,10 @@ for reduced_classes in [True, False]:
 
             assert len(test_pred_classification) == len(test_pred_regression)
 
-
-            # todo: round the classification preds to nearest integer
+            if reduced_classes:
+                random_model = np.random.randint(0, 2, len(test_pred_classification))
+                t, p = ttest_rel(test_pred_classification, random_model)
+                print(f"p-value random model: {p}")
 
             t_stat, p_value = ttest_rel(test_pred_regression, test_pred_classification)
 
