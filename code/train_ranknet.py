39d92882f42968f8388ce1b3bbe68a86f4032e8c
Tom Lotze <tom.lotze@student.auc.nl>
2020-10-21 12:47:26 +0200

    plotting

--- code/train_ranknet.py
+++ code/train_ranknet.py
@@ -263,11 +263,12 @@ def plotting(train_losses, valid_losses, test_loss, variables_string, optimal_ba
     os.makedirs("Images", exist_ok=True)
 
     plt.figure(figsize=(15, 9))
-    steps = np.arange(0, len(valid_losses)) * FLAGS.eval_freq
+    train_steps = np.arange(0, len(train_losses)) * FLAGS.eval_freq
+    valid_steps = np.arange(0, len(valid_losses)) * FLAGS.eval_freq
 
     # plot the losses
-    plt.plot(steps, train_losses, '-', lw=2, label="Training NDCG")
-    plt.plot(steps, valid_losses, '-', lw=3, label="Validation NDCG")
+    plt.plot(train_steps, train_losses, '-', lw=2, label="Training NDCG")
+    plt.plot(valid_steps, valid_losses, '-', lw=3, label="Validation NDCG")
     plt.axhline(test_loss, label="Test NDCG", color="red", lw=3)
     # plt.axvline(optimal_batch, linestyle="dashed", color='red', label="Optimal model", lw=3)
     # plt.title('Losses over training, including final test loss using optimal model')
