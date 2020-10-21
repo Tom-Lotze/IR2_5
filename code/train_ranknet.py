dfdb5fccf99157413982164bb033673dc9450a25
Tom Lotze <tom.lotze@student.auc.nl>
2020-10-21 16:46:18 +0200

    new y limts

--- code/train_ranknet.py
+++ code/train_ranknet.py
@@ -265,6 +265,8 @@ def plotting(train_losses, valid_losses, test_loss, variables_string, optimal_ba
     plt.axhline(test_loss, label="Test NDCG", color="red", lw=3)
     # plt.axvline(optimal_batch, linestyle="dashed", color='red', label="Optimal model", lw=3)
     # plt.title('Losses over training, including final test loss using optimal model')
+
+    plt.ylim((0.4, 0.8))
     plt.xlabel('Batch')
     plt.ylabel('NDCG')
     plt.grid(True)
