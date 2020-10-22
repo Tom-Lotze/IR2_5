54c2ae3e36d4e6bf32f97eeafa98e63aa8a0fdb9
Tom Lotze <lgpu0009@test3.irc.sara.nl.irc.sara.nl>
2020-10-22 11:23:49 +0200

    ccopy predictions back from scratch

--- scripts/ranker.sh
+++ scripts/ranker.sh
@@ -12,14 +12,16 @@ cp -r $HOME/IR2_5 $TMPDIR/
 cd $TMPDIR/IR2_5
 mkdir -p Models
 mkdir -p Images
+mkdir -p Predictions
 
 echo "Ranker started running" | mail $USER
 
 python code/test_cuda_print.py
-python code/train_ranknet.py --plotting 1 --use_preds 0
+python code/train_ranknet.py --plotting 1 --use_preds 0 --nr_epochs 5 --use_preds 1
 
 cp -r Models/* $HOME/IR2_5/Models/
 cp -r Images/* $HOME/IR2_5/Images/
+cp -r Predictions/* $HOME/IR2_5/Predictions/
 
 echo "Ranker finished" | mail $USER
 
